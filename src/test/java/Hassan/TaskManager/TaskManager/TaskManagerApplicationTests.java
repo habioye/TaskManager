59264b7de250d06cb8ba41845892385982c8f881
habioye <78454514+habioye@users.noreply.github.com>
2024-06-14 09:53:14 -0400

    Created testing

--- src/test/java/Hassan/TaskManager/TaskManager/TaskManagerApplicationTests.java
+++ src/test/java/Hassan/TaskManager/TaskManager/TaskManagerApplicationTests.java
@@ -1,13 +1,32 @@
 package Hassan.TaskManager.TaskManager;
 
+import Hassan.TaskManager.TaskManager.Controller.TaskController;
+import Hassan.TaskManager.TaskManager.Entity.Tasks;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class TaskManagerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Tasks task = new Tasks();
+		task.task = "Test";
+		task.description  = "Test Description";
+		TaskController controller = new TaskController();
+		controller.createTask(task);
+		Tasks output = controller.getTakskById(task.id);
+		assertNotNull(output);
+		controller.deleteTask(output.id);
+		output = controller.getTakskById(output.id);
+		assertNull(output);
+
+
+
+
 	}
 
 }
